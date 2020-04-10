6057d6c8925886dbfbec0b9cd7423f7a11039cfb
lilyanlefevre <lilyandu70.7@gmail.com>
2020-04-10 10:52:27 +0200

    test

--- gazette/php/deconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('./bibli_gazette.php');
-
-// démarrage de la session
-session_start();
-
-em_session_exit(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '../index.php');
-
-?>
