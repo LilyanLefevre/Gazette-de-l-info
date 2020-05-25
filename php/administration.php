317e8426f954d94ff780dfde1801aa9707553749
lilyanlefevre <lilyandu70.7@gmail.com>
2020-05-25 19:47:38 +0200

    affichage erreur/succès correctement

--- php/administration.php
+++ php/administration.php
@@ -17,10 +17,16 @@ if(!ll_verifie_authentification() || $_SESSION['user']['administrateur']!=true){
 
 // génération de la page
 ll_aff_entete('Administration', 'Administration');
+//variables globales qui valent 1 en cas de succes
+$success=0;
 
-$erreur=NULL;
 if(isset($_POST['btnValider'])){
 	$erreur=cbl_traitement_admin();
+	if(empty($erreur)==true){
+		$success=1;
+	}
+}else{
+	$erreur=false;
 }
 
 
@@ -46,61 +52,59 @@ function ll_cbl_aff_liste_modifie($nom, $options, $defaut) {
 }
 
 function cbl_aff_admin($erreur){
+	global $success;
+	$bd = ll_bd_connecter();
+	$pseudo=mysqli_real_escape_string($bd, $_SESSION['user']['pseudo']);
 
-$bd = ll_bd_connecter();
-$pseudo=mysqli_real_escape_string($bd, $_SESSION['user']['pseudo']);
 
+	$sql ="SELECT utPseudo, utStatut,COUNT(DISTINCT coID),COUNT(DISTINCT arID),nbCom
+	FROM `utilisateur`
+	LEFT OUTER JOIN `commentaire` ON utPseudo = coAuteur
+	LEFT OUTER JOIN `article` ON utPseudo = arAuteur
+	LEFT OUTER JOIN (
+	    SELECT arAuteur AS maxCom,COUNT(coArticle) AS nbCom
+		FROM `article`
+		LEFT OUTER JOIN `commentaire` ON arID = coArticle
+		GROUP BY arAuteur
+	    ) AS max ON utPseudo = max.maxCom
+	GROUP BY utPseudo
+	ORDER BY utStatut DESC
+	";
 
-$sql ="SELECT utPseudo, utStatut,COUNT(DISTINCT coID),COUNT(DISTINCT arID),nbCom
-FROM `utilisateur`
-LEFT OUTER JOIN `commentaire` ON utPseudo = coAuteur
-LEFT OUTER JOIN `article` ON utPseudo = arAuteur
-LEFT OUTER JOIN (
-    SELECT arAuteur AS maxCom,COUNT(coArticle) AS nbCom
-	FROM `article`
-	LEFT OUTER JOIN `commentaire` ON arID = coArticle
-	GROUP BY arAuteur
-    ) AS max ON utPseudo = max.maxCom
-GROUP BY utPseudo
-ORDER BY utStatut DESC
-";
+	$res = mysqli_query($bd, $sql) or ll_bd_erreur($bd, $sql);
+	$statut[0]='Utilisateur simple';
+	$statut[1]='Rédacteur';
+	$statut[2]='Administrateur';
+	$statut[3]='Rédacteur et administrateur';
 
-$res = mysqli_query($bd, $sql) or ll_bd_erreur($bd, $sql);
-$statut[0]='Utilisateur simple';
-$statut[1]='Rédacteur';
-$statut[2]='Administrateur';
-$statut[3]='Rédacteur et administrateur';
+	echo   '<main>',
+	         '<section>',
+	            '<h2>Liste des utilisateurs</h2>';
 
-echo   '<main>',
-         '<section>',
-            '<h2>Liste des utilisateurs</h2>';
+	ll_aff_erreur_success($erreur,$success);
+	echo '<form action="administration.php" method="POST">',
+	            '<table id="listeUtilisateurs">',
+	            '<tr>',
+				    '<td>Pseudo</td>',
+				    '<td>Statut</td>',
+				    '<td>Nombre de commentaires publiés</td>',
+				    '<td>Nombre d\'articles publiés</td>',
+				    '<td>Nombre moyen de commentaires portant sur les articles qu\'il a publié</td>',
+				'</tr>';
 
-	if(isset($erreur)){
-		echo '<p>',$erreur,'</p>';
-    }
-echo '<form action="administration.php" method="POST">',
-            '<table id="listeUtilisateurs">',
-            '<tr>',
-			    '<td>Pseudo</td>',
-			    '<td>Statut</td>',
-			    '<td>Nombre de commentaires publiés</td>',
-			    '<td>Nombre d\'articles publiés</td>',
-			    '<td>Nombre moyen de commentaires portant sur les articles qu\'il a publié</td>',
-			'</tr>';
-
-while($fetch=mysqli_fetch_assoc($res)){
-	echo '<tr>',
-	'<td>',ll_html_proteger_sortie($fetch['utPseudo']),'</td>',
-	'<td>',ll_cbl_aff_liste_modifie(ll_html_proteger_sortie($fetch['utPseudo']),$statut,ll_html_proteger_sortie($fetch['utStatut'])),'</td>',
-	'<td>',ll_html_proteger_sortie($fetch['COUNT(DISTINCT coID)']),'</td>',
-	'<td>',ll_html_proteger_sortie($fetch['COUNT(DISTINCT arID)']),'</td>';
-	if(ll_html_proteger_sortie($fetch['COUNT(DISTINCT arID)'])!=0){
-		echo '<td>',ll_html_proteger_sortie($fetch['nbCom'])/(double)ll_html_proteger_sortie($fetch['COUNT(DISTINCT arID)']),'</td>';
-	}else{
-		echo '<td>0</td>';
+	while($fetch=mysqli_fetch_assoc($res)){
+		echo '<tr>',
+		'<td>',ll_html_proteger_sortie($fetch['utPseudo']),'</td>',
+		'<td>',ll_cbl_aff_liste_modifie(ll_html_proteger_sortie($fetch['utPseudo']),$statut,ll_html_proteger_sortie($fetch['utStatut'])),'</td>',
+		'<td>',ll_html_proteger_sortie($fetch['COUNT(DISTINCT coID)']),'</td>',
+		'<td>',ll_html_proteger_sortie($fetch['COUNT(DISTINCT arID)']),'</td>';
+		if(ll_html_proteger_sortie($fetch['COUNT(DISTINCT arID)'])!=0){
+			echo '<td>',ll_html_proteger_sortie($fetch['nbCom'])/(double)ll_html_proteger_sortie($fetch['COUNT(DISTINCT arID)']),'</td>';
+		}else{
+			echo '<td>0</td>';
+		}
+		'</tr>';
 	}
-	'</tr>';
-}
 
  echo    '</td></tr>',
             '<tr>',
@@ -119,18 +123,26 @@ while($fetch=mysqli_fetch_assoc($res)){
 
 
 function cbl_traitement_admin(){
-
+	$erreurs=array();
 	$bd = ll_bd_connecter();
 
 	foreach ($_POST as $key => $value) {
+		if($value!=0 && $value!=1 && $value!=2 && $value!=3){
+			$erreurs[]="Statut incorrect";
+			return $erreurs;
+		}
 		$pseudo=$key;
 		$sql="UPDATE utilisateur SET utStatut = '{$value}' WHERE utPseudo = '{$pseudo}'";
 		mysqli_query($bd, $sql) or ll_bd_erreur($bd, $sql);
+		if($value==1 || $value==3){
+			$sql="INSERT INTO `redacteur`(`rePseudo`, `reBio`, `reCategorie`, `reFonction`) VALUES ('{$pseudo}',' ','1',NULL)";
+			mysqli_query($bd, $sql) or ll_bd_erreur($bd, $sql);
+		}
 	}
 
 	mysqli_close($bd);
 
-	return 'Elément(s) modifié';
+	$success=1;
 }
 
 
